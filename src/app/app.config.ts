6ee7eff272bdb2057b7ee29c99fff923c53ada48
azeroual anass <azeroual.anass2015@gmail.com>
2024-09-23 00:14:58 +0200

    "Added withHashLocation to provideRouter in app.config.ts"

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
+  // use hash in route
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes,withHashLocation()),
     provideClientHydration(),
-    provideAnimations()
+    provideAnimations(),
   ]
 };
